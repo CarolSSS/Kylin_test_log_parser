527357a542d7be50b60ce578cbb382d2d59e3837
Chen Song <chens3@illinois.edu>
2022-09-29 18:32:31 -0500

    Add files for parsing single test

---
+++ read_properties.py
@@ -0,0 +1,14 @@
+import os
+import pandas as pd
+
+df = pd.DataFrame(columns =['variable', 'value'])
+lines = open('kylin-defaults.properties', "r").readlines()
+for line in lines:
+    line = line.strip("\n")
+    if "=" in line and line[0] != '#':
+        print(line)
+        q = pd.DataFrame([line.split('=', 1)],columns =['variable', 'value'])
+        print(q)
+        df = pd.concat([df, q])
+
+df.to_csv('1.csv')
