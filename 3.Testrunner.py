7b5fb0defa3b24d7f2ced3880936b6dc0d24ed32
ConstaT99 <slyvestert@outlook.com>
2022-10-27 17:38:21 -0500

    update todo testrunner

--- 3.Testrunner.py
+++ 3.Testrunner.py
@@ -2,24 +2,38 @@ import os
 import json
 from pathlib import Path
 import csv
-
+# to
 ctest_file = "../kylin/core-common/src/main/resources/ctest.properties"
 working_dir = "../kylin"
+git_link = "https://github.com/apache/kylin"
+sha = "63f9ac6bcd0db005f10935d88747d39fc0819ab7"
 
-def file_generate(test_name):
-    json_file = open('./result/map.json')  # param_unset_getter_map path
+# TODO
+# finished file gnerate function
+# finish run all test function
+def file_generate(file_path):
+    file_path = './result/common_map.json'
+    json_file = open(file_path)  # param_unset_getter_map path
     para_map = json.load(json_file)
-    my_para = para_map[test_name]
-    data = {}
-    for i in my_para:
-        data[i] = [[], []]
-    with open("./result/" + test_name + '.csv', 'w') as fp:
-        header = "REPO, SHA, CONFIG_PARAMETER, TEST_NAME, VALUE, TYPE(GOOD|BAD), EXPECTATION(PASS|FAIL)"
-        fp.write("%s\n" % header)
+    csvoutput = []
+    # my_para = para_map[test_name]
+    for test_name in para_map:
+        for i in para_map[test_name]:
+
+            csvoutput.append()
 
 
-def run_ctest(module_name, test_name, config_parameter, value):
-    inject(config_parameter, value)
+    # data = {}
+    # for i in my_para:
+    #     data[i] = [[], []]
+    # with open("./result/" + test_name + '.csv', 'w') as fp:
+    #     header = "REPO, SHA, CONFIG_PARAMETER, TEST_NAME, VALUE, TYPE(GOOD|BAD), EXPECTATION(PASS|FAIL)"
+    #     fp.write("%s\n" % header)
+    pass
+
+
+def run_ctest(module_name, test_name, config_parameter, config_value):
+    inject(config_parameter, config_value)
     command = "mvn -pl " + module_name + " test -Dtest=" + test_name
     print(command)
     os.chdir(working_dir)
@@ -31,9 +45,9 @@ def run_ctest(module_name, test_name, config_parameter, value):
         else:
             result = "FAIL"
     if result == "PASS":
-        print("[myctest]--> " + config_parameter + " " + value + " " + " " + "\033[32m"+result+"\033[0m")
+        print("[myctest]--> " + config_parameter + " " + config_value + " " + " " + "\033[32m"+result+"\033[0m")
     elif result == "FAIL":
-        print("[myctest]--> " + config_parameter + " " + value + " " + " " + "\033[31m"+result+"\033[0m")
+        print("[myctest]--> " + config_parameter + " " + config_value + " " + " " + "\033[31m"+result+"\033[0m")
     return result
 
 
@@ -44,17 +58,18 @@ def file_is_exist(test_name):
 
 def run_all_ctest(git_link, sha, test_name, module_name):
     csv_output = []
+    # need to change to csv files
+    # finished the file generate first
     with open(test_name + '.json') as json_file:
         test_dict = json.load(json_file)
-    os.chdir('../IDoCT/run_ctest')  # ctest function path
     for i in test_dict:
         config_parameter = i
         for j in range(len(test_dict[i][0])):
-            value = test_dict[i][0][j]
+            config_value = test_dict[i][0][j]
             types = test_dict[i][1][j]
             result = run_ctest(module_name, test_name, config_parameter, value, )
-            csv_output.append(git_link + ", " + sha + ", " + config_parameter + ", " + test_name + ", " + value + ", " +
-                              types + ", " + result)
+            csv_output.append(git_link + ", " + sha + ", " + config_parameter + ", " + test_name + ", " + config_value +
+                              ", " + types + ", " + result)
     print("")
     os.chdir('../../utils')  # result dir
     file_name = test_name + ".csv"
@@ -66,11 +81,11 @@ def run_all_ctest(git_link, sha, test_name, module_name):
         print('Done')
 
 
-def inject(name, value):
+def inject(name, config_value):
     with open(ctest_file, 'w') as fp:
         print("ctest file at : " + ctest_file)
-        print("inject parameter: " + name + " = " + value)
-        fp.write(name + " =" + value)
+        print("inject parameter: " + name + " = " + config_value)
+        fp.write(name + " =" + config_value)
 
 
 def delete():
@@ -81,8 +96,9 @@ def delete():
 
 
 if __name__ == "__main__":
-    module = "core-common"
-    testName = "KylinServerDiscoveryTest#test"
-    param = "kylin.env.zookeeper-base-sleep-time"
-    value = "5555"
-    run_ctest(module, testName, param, value)
+    # module = "core-common"
+    # testName = "KylinServerDiscoveryTest#test"
+    # param = "kylin.env.zookeeper-base-sleep-time"
+    # value = "5555"
+    # run_ctest(module, testName, param, value)
+    file_generate("1")
